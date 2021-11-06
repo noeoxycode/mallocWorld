951c7eafef4afda575e19718ef4f9726896e571c
Lucas Leclerc <leclerclucas@neuf.fr>
2021-11-06 12:57:49 +0100

    Creation du generateur de list

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "map_creator.c"
 int main() {
-    int  **map = createMap(12, 12);
-    displayMap(map, 12, 12);
+    int  ***listMap = listMapGenerator();
+    for (int i=1;i<4;i++) {
+        displayMap(listMap[i - 1], i);
+        printf("--------------------\n");
+    }
+
     return 0;
 }
--- map_creator.c
+++ map_creator.c
@@ -7,11 +7,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int ** createMap(int level);
+void displayMap(int **map,int level);
+int *** listMapGenerator();
+int getMapSize(int level);
+int **fillWall(int **map, int mapLevel);
+int **fillItems(int **map, int mapLevel);
+int **fillMob(int **map, int mapLevel);
+
+int getMapSize(int level){
+    if(level==1){
+        return 12;
+    }else if(level==2){
+        return 15;
+    }else{
+        return 20;
+    }
+}
+
+int *** listMapGenerator(){
+    int **map1= createMap(1);
+    int **map2= createMap(2);
+    int **map3= createMap(3);
+    int ***listMap= malloc(sizeof(int**)*3);
+    listMap[0]=map1;
+    listMap[1]=map2;
+    listMap[2]=map3;
+    return listMap;
+}
 
 //prend une map en param et l'affiche
-void displayMap(int **map,int height,int width){
-    for(int y=0;y<height;y++){
-        for(int x=0;x<width;x++){
+void displayMap(int **map,int level){
+    int size=getMapSize(level);
+    for(int y=0;y<size;y++){
+        for(int x=0;x<size;x++){
+            if(map[x][y]<10){
+                printf(" ");
+            }
             printf("%d ",map[x][y]);
         }
         printf("\n");
@@ -19,16 +51,20 @@ void displayMap(int **map,int height,int width){
 }
 
 //crée une carte et la retourne
-int ** createMap(int height,int width){
-    int** map=malloc(sizeof(int*)*height);
-    for(int x=0;x<width;x++){
-        map[x]=malloc(sizeof(int)*width);
+int ** createMap(int level){
+    int size= getMapSize(level);
+    int** map=malloc(sizeof(int*)*size);
+    for(int x=0;x<size;x++){
+        map[x]=malloc(sizeof(int)*size);
     }
-    for(int y=0;y<height;y++){
-        for(int x=0;x<width;x++){
+    for(int y=0;y<size;y++){
+        for(int x=0;x<size;x++){
             map[y][x]=0;
         }
     }
+    map=fillWall(map,level);
+    map=fillItems(map,level);
+    map=fillMob(map,level);
     return map;
 }
 
@@ -37,6 +73,7 @@ int **fillWall(int **map, int mapLevel){
     //determine le nb de murs
 
     //place les murs
+    return map;
 }
 
 //remplis la map avec les mob
@@ -44,6 +81,7 @@ int **fillMob(int **map, int mapLevel){
     //determine le nb de mob
 
     //place les mob
+    return map;
 }
 
 //remplis la map avec les items
@@ -51,4 +89,5 @@ int **fillItems(int **map, int mapLevel){
     //determine le nb de items
 
     //place les items
+    return map;
 }
