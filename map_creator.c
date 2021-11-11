0aae417fad00e8ae2be54bc8b7680d89dea3c38d
noeoxycode <noe@oxycode.io>
2021-11-11 15:54:22 +0100

    map finish

--- map_creator.c
+++ map_creator.c
@@ -41,7 +41,7 @@ void displayMap(int **map,int level){
     int size=getMapSize(level);
     for(int y=0;y<size;y++){
         for(int x=0;x<size;x++){
-            if(map[x][y]<10){
+            if(map[x][y]<10 &&map[x][y]!=-1){
                 printf(" ");
             }
             printf("%d ",map[x][y]);
@@ -63,15 +63,32 @@ int ** createMap(int level){
         }
     }
     map=fillWall(map,level);
+    printf("test 1");
     map=fillItems(map,level);
+    printf("test 2");
     map=fillMob(map,level);
+    printf("test 3");
     return map;
 }
 
 //remplis la map avec les murs
 int **fillWall(int **map, int mapLevel){
     //determine le nb de murs
-
+    srand(time(0));
+    int x;
+    int y;
+    int nbMurs = rand() % (mapLevel * 10)+1;
+    int i = 0;
+    while (i < nbMurs)
+    {
+        x = rand() % getMapSize(mapLevel);
+        y = rand() % getMapSize(mapLevel);
+        if (map[x][y] == 0)
+        {
+            map[x][y] = -1;
+            i++;
+        }
+    }
     //place les murs
     return map;
 }
@@ -79,15 +96,58 @@ int **fillWall(int **map, int mapLevel){
 //remplis la map avec les mob
 int **fillMob(int **map, int mapLevel){
     //determine le nb de mob
+    int nbMob = 0;
+    if (mapLevel == 1)
+        nbMob = 10;
+    else if (mapLevel == 2)
+        nbMob = 13;
+    else if (mapLevel == 3)
+        nbMob = 17;
+    else
+        nbMob = -1;
 
-    //place les mob
+    int i = 0;
+    srand(time(0));
+    int randomMonster=0,x=0,y=0;
+    while (i < nbMob)
+    {
+        randomMonster = ( rand() % 15 ) + 12;
+        x = rand()% (getMapSize(mapLevel));
+        y = rand()% (getMapSize(mapLevel));
+        if (map[x][y] == 0)
+        {
+            map[x][y] = randomMonster;
+            i++;
+        }
+    }
     return map;
 }
 
 //remplis la map avec les items
 int **fillItems(int **map, int mapLevel){
-    //determine le nb de items
-
-    //place les items
+    srand(time(0));
+    int x;
+    int y;
+    int cpt = 0;
+    int typeItem = 3 * mapLevel;
+    int *numbersOfMapObjects = (int *)malloc(sizeof(int)*3);
+    numbersOfMapObjects[0] =rand()% (10);
+    numbersOfMapObjects[1] =rand()% (10);
+    numbersOfMapObjects[2] =rand()% (10);
+    for (int i = 0; i < 3; i++)
+    {
+        while (cpt < numbersOfMapObjects[i])
+        {
+            x = rand()% (getMapSize(mapLevel));
+            y = rand()% (getMapSize(mapLevel));
+            if (map[x][y] == 0)
+            {
+                map[x][y] = typeItem;
+                cpt++;
+            }
+        }
+        cpt = 0;
+        typeItem++;
+    }
     return map;
 }
